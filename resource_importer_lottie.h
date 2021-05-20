055e21bf08f23648579f8a0ee4f6cc953ff1c984
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-05-20 09:52:15 -0700

    Disable lottie.

--- resource_importer_lottie.h
+++ resource_importer_lottie.h
@@ -32,7 +32,7 @@
 
 #include "core/io/resource_importer.h"
 #include "core/io/resource_saver.h"
-#include "scene/3d/mesh_instance.h"
+#include "scene/3d/mesh_instance_3d.h"
 #include "scene/3d/spatial.h"
 #include "scene/resources/packed_scene.h"
 #include "scene/resources/primitive_meshes.h"
