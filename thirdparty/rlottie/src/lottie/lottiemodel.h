2d4a265543d46663f89884692ea5697d62ad5229
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-08-04 07:09:38 -0700

    Unused change.

--- thirdparty/rlottie/src/lottie/lottiemodel.h
+++ thirdparty/rlottie/src/lottie/lottiemodel.h
@@ -622,7 +622,7 @@ public:
         if (isStatic()) return impl.mStaticData.mOpacity;
         return impl.mData->opacity(frameNo);
     }
-    Transform(const Transform3D &) = delete;
+    Transform(const Transform &) = delete;
     Transform(Transform &&) = delete;
     Transform &operator=(Transform &) = delete;
     Transform &operator=(Transform &&) = delete;
